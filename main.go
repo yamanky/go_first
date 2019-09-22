8286f745ed4ec685f3bb370c33a55693bdd340fd
山内泰孝 <yamauchi@yamankysMBP.local>
2019-09-22 21:18:32 +0900

    complete API

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-
 	"github.com/go_first/db"
+	"github.com/go_first/server"
 )
 
 func main() {
-    r := gin.Default()
-    r.GET("/", func(c *gin.Context) {
-        c.String(200, "Hello, World")
-	})
 	db.Init()
-    r.Run()
+    server.Init()
 }
---
+++ server/server.go
@@ -0,0 +1,30 @@
+package server
+
+
+import (
+    "github.com/gin-gonic/gin"
+
+    "github.com/go_first/controller"
+)
+
+// Init is initialize server
+func Init() {
+    r := router()
+    r.Run()
+}
+
+func router() *gin.Engine {
+    r := gin.Default()
+
+    u := r.Group("/users")
+    {
+        ctrl := user.Controller{}
+        u.GET("", ctrl.Index)
+        u.GET("/:id", ctrl.Show)
+        u.POST("", ctrl.Create)
+        u.PUT("/:id", ctrl.Update)
+        u.DELETE("/:id", ctrl.Delete)
+    }
+
+    return r
+}
